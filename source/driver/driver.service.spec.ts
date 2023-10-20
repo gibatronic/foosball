3894757ad04197d494b75cc867dc2a2715c5260f
Gibran Malheiros <gibatronic@gmail.com>
2023-10-20 20:58:44 +0200

    fixed tests

--- source/driver/driver.service.spec.ts
+++ source/driver/driver.service.spec.ts
@@ -1,6 +1,8 @@
+jest.mock('@nestjs/config')
 jest.mock('../logger/logger.service')
 jest.mock('../teams/teams.service')
 
+import { ConfigService } from '@nestjs/config'
 import { EventEmitter2 } from '@nestjs/event-emitter'
 import { Test, TestingModule } from '@nestjs/testing'
 import { LoggerService } from '../logger/logger.service'
@@ -13,6 +15,7 @@ describe('DriverService', () => {
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             providers: [
+                ConfigService,
                 DriverService,
                 EventEmitter2,
                 LoggerService,
--- source/scoreboard/scoreboard.service.spec.ts
+++ source/scoreboard/scoreboard.service.spec.ts
@@ -1,10 +1,12 @@
 jest.mock('@nestjs/config')
+jest.mock('../driver/driver.service')
 jest.mock('../logger/logger.service')
 jest.mock('../teams/teams.service')
 
 import { ConfigService } from '@nestjs/config'
 import { EventEmitter2 } from '@nestjs/event-emitter'
 import { Test, TestingModule } from '@nestjs/testing'
+import { DriverService } from '../driver/driver.service'
 import { LoggerService } from '../logger/logger.service'
 import { TeamsService } from '../teams/teams.service'
 import { ScoreboardService } from './scoreboard.service'
@@ -16,6 +18,7 @@ describe('ScoreboardService', () => {
         const module: TestingModule = await Test.createTestingModule({
             providers: [
                 ConfigService,
+                DriverService,
                 EventEmitter2,
                 LoggerService,
                 ScoreboardService,
